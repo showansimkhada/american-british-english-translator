9376a0c9f65f25f67d5b4b471546d3c3cf556429
showansimkhada <showansimkhada@hotmail.com>
2023-03-18 07:35:50 +1300

    things to do

--- components/translator.js
+++ components/translator.js
@@ -2,9 +2,38 @@ const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
-
+var AO = new Object(americanOnly);
+var ATBS = new Object(americanToBritishSpelling);
+var ATBT = new Object(americanToBritishTitles);
+var BO = new Object(britishOnly);
+    
 class Translator {
+    traTAm(str) {
+        console.log("Translating to American")
+        var result = '';
+        let strSplit = str.split(' ');
+        for (let i = 0; i < strSplit.length; i++) {
+            if (AO.hasOwnProperty(strSplit[i])) {
+                result += ATBS[strSplit[i]] + " ";
+            }
+            else {
+                result += strSplit[i]+" ";
+            }
+        }
+        return result;
+    }
 
+    traTBr(str) {
+        console.log("Translating to British")
+        var result = '';
+        if (ATBS.hasOwnProperty(str)) {
+            result += ATBS[str] + " ";
+        }
+        else if (BO.hasOwnProperty(str)){
+            result += BO[str];
+        }
+        return result;
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
@@ -8,6 +7,39 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      let locale = req.body.locale;
+      var text = req.body.text;
+      if (!text) {
+        res.json({
+          "error": "No text to translate"
+        })
+      } else {
+        // translate to british
+        if (locale === 'american-to-british') {
+          let result = translator.traTBr(text);
+          if (result.split() === text.split()) {
+            console.log('same')
+          }
+          res.json({
+            translation: result
+          })
+        } else {
+          // translate to american
+          // Split the texts non charaters
+          // Split words and go through every words
+          // If it begain with capital letters and result is same then output should be eveythings look good
+          // else just output all
+          // all output first word starts with capital letter
+          // all translated words should be highlighted
+          let result = translator.traTAm(text);
+          console.log(result)
+          if (result.split() == text.split()) {
+            console.log('same')
+          }
+          res.json({
+            translation: result
+          })
+        }
+      }
     });
 };
